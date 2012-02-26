9b9274bc3a50ce71b3611ec5c7fc5a81723bd222
John B. Petersen III <john.b.petersen@gmail.com>
2012-02-25 21:59:11 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CandleMentors::Application.config.secret_token = '6efcdf379edf04d648d4cf3f4816d9550d3ce270b5004666d00390e17fba0cfc75cf09b542eaef1b79b21606fb5dc31bbdc98d7de1fb3e2b4e830cfb9bd672b5'
